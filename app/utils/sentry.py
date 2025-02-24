0fc01824cb3a83e5f4aa5b623cbd9c03290c9319
Julien Denizot <luunz71@gmail.com>
2025-02-24 17:07:04 +0100

    add condition for sentry

--- app/utils/sentry.py
+++ app/utils/sentry.py
@@ -9,10 +9,10 @@ SENTRY_DSN = os.getenv("SENTRY_DSN")
 if not SENTRY_DSN:
     print("❌ SENTRY_DSN non défini. Sentry ne sera pas activé.")
 
-
-sentry_sdk.init(
-    dsn=SENTRY_DSN,
-    send_default_pii=True,
-    traces_sample_rate=1.0
-)
-print("✅ Sentry est activé.")
+else:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        send_default_pii=True,
+        traces_sample_rate=1.0
+        )
+    print("✅ Sentry est activé.")
